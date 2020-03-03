b45847b465cf2c634c2683fb1a1f39cfec86bc6a
James Hebert <onemanpowertrip@gmail.com>
2020-03-03 15:01:47 -0600

    Dungeon Test Environment

---
+++ Assets/Project Scripts/Unit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+    public int perception;
+    public int armorClass;
+    public int currentHP;
+    public int maxHP;
+    public int meleeAttackBonus;
+    public int rangedAttackBonus;
+    public int spellAttackBonus;
+    public int meleeDamage;
+    public int rangedDamage;
+    public int speed;
+
+   public bool TakeDamage(int dmg)
+    {
+        currentHP -= dmg;
+
+        if (currentHP <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void Heal(int amountHealed)
+    {
+        currentHP += amountHealed;
+        if (currentHP > maxHP)
+            currentHP = maxHP;
+    }
+}
